9f8702683bcf8d2980830354d1486c4814a0002e
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-09-14 17:30:10 +0530

    responding to URL paramenters

--- app.js
+++ app.js
@@ -18,6 +18,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
@@ -36,6 +57,7 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+  // res.send('Done');
 });
 
 const port = 4000;
