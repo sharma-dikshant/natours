ace91c319e4fcc4db2d5931afadaafe991a2081c
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-09-17 22:31:49 +0530

    setup mongoose, connect database and created first schema and model for tour

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<DB_PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('DB connection successful!');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 // console.log(app.get('env'));
 // console.log(process.env);
 
