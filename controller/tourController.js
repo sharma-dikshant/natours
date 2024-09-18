cb6ae3d3fc4dacb292d513a2cd7280216f433d7a
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-09-18 22:13:42 +0530

    implemented advanced filtering functionalites using gte, gt, lte, lt operators

--- controller/tourController.js
+++ controller/tourController.js
@@ -2,15 +2,23 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     //! BUILD QUERY
+    //! 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit'];
     excludedFields.forEach((el) => delete queryObj[el]);
-    // console.log(req.query, queryObj);
-    // const tours = await Tour.find(req.query || {});
-    // const tours = await Tour.find(queryObj);
-    const query = Tour.find(queryObj);
 
+    //! 2) Advanced Filtering
+    //for implementing gte, gt, lte, lt
+    // { difficulty: 'easy', duration: { $gte: 5 } } => required query
+    // { difficulty: 'easy', duration: { gte: 5 } } => query from user which is almost same as required query
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
     //! EXECUTE QUERY
     const tours = await query;
 
