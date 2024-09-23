23a5678539e6546e692644f4e98291c8f0810391
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-09-23 09:17:05 +0530

    implemented aggregation pipeline unwinding and projecting.

--- routes/tourRouters.js
+++ routes/tourRouters.js
@@ -7,6 +7,7 @@ const {
   getTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require('./../controller/tourController');
 
 const router = express.Router();
@@ -14,6 +15,7 @@ const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
