24bfd9b10ebe0c9e1520dc6e2d510fbf81be86d3
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-09-25 17:59:23 +0530

    handled unhandled routes

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware 👋');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -25,5 +25,14 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//! handling unhandled routes
+// if this middleware is reached, it means that the request was not handled by any of the routers above
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
