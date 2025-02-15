0e2f9068a6658d51744c02af7d899b9cd3e1e1c2
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-15 13:46:12 +0530

    refactored tour model

---
+++ models/tourModel.js
@@ -0,0 +1,19 @@
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    requried: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
--- server.js
+++ server.js
@@ -28,38 +28,6 @@ mongoose
 // console.log(app.get('env'));
 // console.log(process.env);
 
-const tourSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    requried: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hiker TEST',
-  rating: 4.7,
-  price: 497,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('Error:', err);
-  });
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
