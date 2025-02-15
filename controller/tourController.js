e62a5d2c182f8eb5831428330980f1096117429b
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-15 16:08:46 +0530

    U: updated the tour doc

--- controller/tourController.js
+++ controller/tourController.js
@@ -55,11 +55,25 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: '<Updated Tour here... >',
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // return the new updated data
+      runValidators: true, // run the validators again
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
