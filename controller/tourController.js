1c4f458ea3a3f99cd4b784ac34f4742c00f9663f
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-17 12:28:16 +0530

    implemented filters

--- controller/tourController.js
+++ controller/tourController.js
@@ -2,7 +2,17 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //? BUILD QUERY
+    const queryObj = { ...req.query };
+    const deletedFields = ['page', 'sort', 'limit', 'fields'];
+    deletedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    //? EXECUTE QUERY
+    const tours = await query;
+
+    //? SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
