124255d623995662c236eb80fea15b37412775ec
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-18 21:45:02 +0530

    handled errors in prod and dev, seperately

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,9 +1,35 @@
+const sendErrorDev = (err, res) => {
+  //Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    //Programming or other known error: don't leak error details to client
+    res.status(500).json({
+      status: 'error',
+      message: 'something went very wrong!',
+    });
+  }
+};
+
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'Error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev();
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd();
+  }
 };
