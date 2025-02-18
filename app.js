081b2a6d6f339119414fe4ba23640c4c24711ec9
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-18 16:07:48 +0530

    refactored global error handler

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controller/errorController');
 const tourRouter = require('./routes/tourRouters');
 const userRouter = express.Router('./routes/userRouters');
+
+const app = express();
 
 //! 1) MIDDLEWARES
 console.log(process.env.NODE_ENV);
@@ -10,40 +14,16 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.use(express.json());
-
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // console.log(req.url);
-  // console.log(req.originalUrl);
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`,
-  // });
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.statusCode = 404;
-  err.status = 'fail';
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'Error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
