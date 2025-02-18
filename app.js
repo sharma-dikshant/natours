15ec8ebee255b233e9f56be0e94ab8ba2491cfc7
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-18 15:35:19 +0530

    handled unhandled routes

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -21,5 +20,14 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // console.log(req.url);
+  // console.log(req.originalUrl);
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
