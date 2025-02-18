0049766018ace75b34d5f11dad02f4242f3de819
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-18 09:28:34 +0530

    apply doc middleware to slugify the name

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+
 const tourSchema = mongoose.Schema(
   {
     name: {
@@ -7,6 +9,7 @@ const tourSchema = mongoose.Schema(
       // unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       requried: [true, 'A tour must have a duration'],
@@ -63,6 +66,23 @@ const tourSchema = mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+//DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  console.log(this);
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save doc....');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
