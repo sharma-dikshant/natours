ef90940a4a6e22084067a93a722740da3ee30ba9
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-19 09:47:35 +0530

    add uncaught exception handler to gracefully shut down the server

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED EXCEPTION! 💥 Shutting down...');
+  process.exit(1);
+});
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
