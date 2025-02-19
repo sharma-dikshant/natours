ea4395663130b09fe156cdf80a66469447297379
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-19 17:19:25 +0530

    add login functionality and refactor token generation in authController

--- models/userModel.js
+++ models/userModel.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -45,6 +46,14 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+//this is an instance method which will be available on all documents in a certain collection
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
