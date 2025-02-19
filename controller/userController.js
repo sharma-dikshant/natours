914270a3b072634f1d82ebe33a02a67e47ffe70f
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-19 17:19:12 +0530

    implement getAllUsers function and add login route

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,15 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const User = require('../models/userModel');
+
+exports.getAllUsers = async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      users,
+    },
   });
 };
 exports.getUser = (req, res) => {
