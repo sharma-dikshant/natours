4fe751b23417080b2f1558f2187be18eabf61517
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-20 16:15:30 +0530

    implement update user information functionality with input filtering

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,15 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const User = require('../models/userModel');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
 exports.getAllUsers = async (req, res, next) => {
   const users = await User.find();
 
@@ -12,6 +21,33 @@ exports.getAllUsers = async (req, res, next) => {
     },
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'this route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+
+  //2) Update user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
