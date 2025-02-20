cc048671d7bf3850b5dbe8d0b1c84b950f07a603
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-20 23:25:01 +0530

    add helmet for security

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
@@ -10,22 +11,31 @@ const userRouter = require('./routes/userRouters');
 const app = express();
 
 //! 1) GLOBAL MIDDLEWARES
+
+//Set security HTTP headers
+app.use(helmet());
+
+//Development logging
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//ratilimiting
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
-
 app.use('/api', limiter);
 
-app.use(express.json());
+//body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
   // console.log(req.headers);
   next();
