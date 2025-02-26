bf1baf772eb4a3f12e12945a28db495f6cc87732
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-26 11:49:09 +0530

    Experimenting: Implement Embedding in tours for user (guides)

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 
 const tourSchema = mongoose.Schema(
   {
@@ -102,6 +103,7 @@ const tourSchema = mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -117,6 +119,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   // console.log(this); this->doc
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
