d9c9e80ad1fd35de4e6df014898067d576bc138d
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-28 01:28:04 +0530

    implemented schema for Reviews

---
+++ models/reviewModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const rewiewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "A review can't be empty."],
+    },
+    rating: {
+      type: Number,
+      default: 1,
+      min: [1, 'Rating must be greater or equal 1'],
+      max: [5, 'Rating must be less or equal 5'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.'],
+    },
+    author: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a author.'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model('review', rewiewSchema);
+
+module.exports = Review;
