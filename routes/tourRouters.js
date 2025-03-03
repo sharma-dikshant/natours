62e2cca24e4f367deb7bc5311743bb813b5fb4db
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-03-03 23:04:34 +0530

    Add getDistances function to calculate distances between tours and a given point

--- routes/tourRouters.js
+++ routes/tourRouters.js
@@ -28,6 +28,8 @@ router
 //tour-within/20/center/-24.554,234.555/unit/km
 // or tour-within?distance=20&center=-24.554,234.555&unit=km
 
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router
   .route('/')
   .get(tourController.getAllTours)
