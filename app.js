050f977074a95ecb9f288eccd6acb511a27330e4
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-03-04 00:59:51 +0530

    some coding fundamentals of pugs

--- app.js
+++ app.js
@@ -71,7 +71,10 @@ app.use((req, res, next) => {
 
 //routes
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: "The Himanlyan Rides",
+    user: "dikshant"
+  });
 });
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
