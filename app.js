d89f05caf086196b5f88252f37d28d531571112e
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-03-04 00:35:48 +0530

    Set up Pug as the view engine

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,6 +14,9 @@ const userRouter = require('./routes/userRouters');
 const reviewRouter = require('./routes/reviewRouters');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 //! 1) GLOBAL MIDDLEWARES
 
@@ -65,6 +69,10 @@ app.use((req, res, next) => {
   next();
 });
 
+//routes
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
