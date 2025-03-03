e4f5e44f3348f9880a07cc990b6f4c0ccc76e776
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-03-03 09:48:07 +0530

    calulating avg Rating on tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const rewiewSchema = new mongoose.Schema(
   {
@@ -46,6 +47,34 @@ rewiewSchema.pre(/^find/, function (next) {
     },
   ]);
   next();
+});
+
+//static method for calculating avgRating
+rewiewSchema.statics.calAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  console.log(stats);
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsAverage: stats[0].avgRating,
+    ratingsQuantity: stats[0].nRating,
+  });
+};
+
+rewiewSchema.post('save', function () {
+  //this points to current review doc and this.constructor points to current model
+  this.constructor.calAverageRatings(this.tour);
+  // next();    //post middleware does'nt have access to next function
 });
 
 const Review = mongoose.model('Review', rewiewSchema);
--- models/tourModel.js
+++ models/tourModel.js
@@ -138,22 +138,6 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-// tourSchema.pre('save', async function (next) {
-//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-//   this.guides = await Promise.all(guidesPromises);
-//   next();
-// });
-
-// tourSchema.pre('save', function (next) {
-//   console.log('Will save doc....');
-//   next();
-// });
-
-// tourSchema.post('save', function (doc, next) {
-//   console.log(doc);
-//   next();
-// });
-
 //QUERY MIDDLEWARE:
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
