43a17b95a37eb316d49d87a3866a9c87b13c2c60
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-03-12 18:21:36 +0530

    build tour overview and dynamically rendered tours details

--- controller/viewController.js
+++ controller/viewController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1) Get Tour Data from collection
+  const tours = await Tour.find();
+
+  //2) Build templete
+  //3) Render that template using tour data
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('overview', {
